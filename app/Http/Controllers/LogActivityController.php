2bb2ebbe2942b97ff237586245b3e0c2c2e2e10a
Kevin Abrar Khansa <kevariable@gmail.com>
2020-08-22 21:59:05 +0700

    Finished

--- app/Http/Controllers/LogActivityController.php
+++ app/Http/Controllers/LogActivityController.php
@@ -8,5 +8,25 @@ class LogActivityController extends Controller
 {
     public function export()
     {
+        $logs = LogActivity::orderBy('time')->cursor();
+        $filename = 'log-activities.cvs';
+
+        return response()->streamDownload(function () use ($logs) {
+            $csv = fopen("php://output", "w+");
+
+            fputcsv($csv, ["Time", "User ID", "Message", "IP Address", "User Agent"]);
+
+            foreach ($logs as $log) {
+                fputcsv($csv, [
+                    $log->time,
+                    $log->user_id,
+                    $log->message,
+                    $log->ip_address,
+                    $log->user_agent
+                ]);
+            }
+
+            fclose($csv);
+        }, $filename, ['Content-type' => 'text/cvs']);
     }
 }
